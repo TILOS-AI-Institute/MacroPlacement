b5791430744876a3a9e83b890bb43f2253f1486c
ZhiangWang033 <69448998+ZhiangWang033@users.noreply.github.com>
2022-06-06 08:51:38 -0700

    Update gen_mp_config.tcl

--- Flows/NanGate45/mempool_tile/scripts/cadence/gen_mp_config.tcl
+++ Flows/NanGate45/mempool_tile/scripts/cadence/gen_mp_config.tcl
@@ -1,3 +1,7 @@
+# This script was written and developed by OpenROAD (or UCSD), however, the underlying commands and reports are copyrighted by Cadence. 
+# We thank Cadence for granting permission to share our research to help promote and foster the next generation of innovators.
+
+
 puts "VERSION 1.0"
 set mem_hier ""
 foreach a [dbget [dbget top.insts.cell.name fakeram45_* -p2 ].name ] {
