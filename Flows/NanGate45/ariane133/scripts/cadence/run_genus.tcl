984f98cd3988653f1fe36b61a0771f3f0f9351b3
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-06-11 03:36:03 -0700

    Upddate scripts for ariane133
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- Flows/NanGate45/ariane133/scripts/cadence/run_genus.tcl
+++ Flows/NanGate45/ariane133/scripts/cadence/run_genus.tcl
@@ -1,15 +1,29 @@
-# This script was written and developed by ABKGroup students at UCSD; however, the underlying commands and reports are copyrighted by Cadence. 
+# This script was written and developed by ABKGroup students at UCSD. However, the underlying commands and reports are copyrighted by Cadence. 
 # We thank Cadence for granting permission to share our research to help promote and foster the next generation of innovators.
-set top_module ariane 
+source lib_setup.tcl
+source design_setup.tcl
+
+# set the output directories
+set OUTPUTS_PATH  syn_output
+set REPORTS_PATH  syn_rpt
+set HANDOFF_PATH  syn_handoff
+
+if {![file exists ${OUTPUTS_PATH}]} {
+  file mkdir ${OUTPUTS_PATH}
+}
+
+if {![file exists ${REPORTS_PATH}]} {
+  file mkdir ${REPORTS_PATH}
+}
+
+if {![file exists ${HANDOFF_PATH}]} {
+  file mkdir ${HANDOFF_PATH}
+}
+#
+# set threads
 set_db max_cpus_per_server 16
-set libdir "../../../../../Enablements/NanGate45/lib/"
-
-set libworst "
-    ${libdir}/NangateOpenCellLibrary_typical.lib \
-    ${libdir}/fakeram45_256x16.lib \
-    ${libdir}/fakeram45_256x64.lib \
-    "
-
+set_db super_thread_servers "localhost"
+#
 set list_lib "$libworst"
 
 # Target library
@@ -17,51 +31,70 @@ set link_library $list_lib
 set target_library $list_lib
 
 # set path
-set_db auto_ungroup none 
-set_db init_lib_search_path $libdir
-set_db init_hdl_search_path "../../rtl/"
+set_db auto_ungroup none
 
 set_db library $list_lib
-set_dont_touch fakeram45_256x16
 
-# Start
-if {[file exists template]} {
-	exec rm -rf template
+
+#################################################
+# Load Design and Initialize
+#################################################
+
+source rtl_list.tcl
+
+foreach rtl_file $rtl_all {
+    read_hdl -sv $rtl_file
 }
 
-exec mkdir template
-if {![file exists gate]} {
-	exec mkdir gate
-}
+elaborate $DESIGN
+time_info Elaboration
 
-if {![file exists rpt]} {
-	exec mkdir rpt
-}
+read_sdc $sdc
+init_design
 
-# Compiler drectives
-set compile_effort "high"
-set compile_flatten_all 1
-set compile_no_new_cells_at_top_level false
+check_design -unresolved
 
-# read RTL
-read_hdl ../../rtl/ariane.v 
+check_timing_intent
+# reports the physical layout estimation report from lef and QRC tech file
+report_ple > ${REPORTS_PATH}/ple.rpt 
 
-elaborate $top_module 
-
-
-# Default SDC Constraints
-read_sdc ../../constraints/${top_module}.sdc
+# keep hierarchy during synthesis
+set_db auto_ungroup none
 
 syn_generic
+time_info GENERIC
+
+# generate a summary for the current stage of synthesis
+write_reports -directory ${REPORTS_PATH} -tag generic
+write_db  ${OUTPUTS_PATH}/${DESIGN}_generic.db
+
 syn_map
+time_info MAPPED
 
-write_sdc > gate/${top_module}.sdc
-write_hdl > gate/${top_module}.v
-write_script > constraints.g
+# generate a summary for the current stage of synthesis
+write_reports -directory ${REPORTS_PATH} -tag map
+write_db  ${OUTPUTS_PATH}/${DESIGN}_map.db
 
-# Write Reports
-redirect [format "%s%s%s" rpt/ $top_module _area.rep] { report_area }
-redirect [format "%s%s%s" rpt/ $top_module _cell.rep] { report_gates }
-redirect [format "%s%s%s" rpt/ $top_module _timing.rep] { report_timing }
+syn_opt
+time_info OPT 
+write_db ${OUTPUTS_PATH}/${DESIGN}_opt.db
+
+##############################################################################
+# Write reports
+##############################################################################
+
+# summarizes the information, warnings and errors
+report_messages > ${REPORTS_PATH}/${DESIGN}_messages.rpt
+
+# generate PPA reports
+report_gates > ${REPORTS_PATH}/${DESIGN}_gates.rpt
+report_power > ${REPORTS_PATH}/${DESIGN}_power.rpt
+report_area > ${REPORTS_PATH}/${DESIGN}_power.rpt
+write_reports -directory ${REPORTS_PATH} -tag final 
+
+write_sdc >${HANDOFF_PATH}/${DESIGN}.sdc
+write_hdl > ${HANDOFF_PATH}/${DESIGN}.v
+#write_design -innovus -base_name ${HANDOFF_PATH}/${DESIGN}
 
 exit
+
