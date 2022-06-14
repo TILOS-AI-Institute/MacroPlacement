d0878691d16472dc7f2a0e9eb60f011007d9631a
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-06-14 08:34:40 -0700

    Add mempool_goup flows
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

---
+++ Flows/NanGate45/mempool_group/scripts/cadence/design_setup.tcl
@@ -0,0 +1,17 @@
+# This script was written and developed by ABKGroup students at UCSD. However, the underlying commands and reports are copyrighted by Cadence. 
+# We thank Cadence for granting permission to share our research to help promote and foster the next generation of innovators.
+
+set DESIGN mempool_group_wrap 
+set rtldir ../../../../../Testcases/mempool/rtl
+set sdc  ../../constraints/${DESIGN}.sdc
+
+# def file with die size and placed IO pins
+set floorplan_def ../../def/mempool_group_wrap_fp.def
+#
+# Effort level during optimization in syn_generic -physical (or called generic) stage
+# possible values are : high, medium or low
+set GEN_EFF medium
+
+# Effort level during optimization in syn_map -physical (or called mapping) stage
+# possible values are : high, medium or low
+set MAP_EFF high
