5ed919c2ce399a8aebdd429721e91505b5f1b8b5
ZhiangWang033 <69448998+ZhiangWang033@users.noreply.github.com>
2022-06-20 17:44:00 -0700

    Update test3.py

--- CodeElements/FormatTranslators/test/Bookshelf2ProtocolBufferFormat/test3.py
+++ CodeElements/FormatTranslators/test/Bookshelf2ProtocolBufferFormat/test3.py
@@ -5,7 +5,7 @@ from FormatTranslators import Bookshelf2LefDef2ProBufFormat
 
 if __name__ == '__main__':
     # set up inputs
-    benchmark_dir = './ISPD2012_testcases'
+    benchmark_dir = './DAC_testcases'
     design = 'superblue19'
 
     # other parameters
