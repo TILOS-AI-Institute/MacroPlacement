8b81963b66f4f2002c254cb3b5f24902917ee41e
ZhiangWang033 <zhw033@ucsd.edu>
2022-07-11 13:25:22 -0700

    add halo_width to gridding

--- CodeElements/Gridding/test/test_gridding.py
+++ CodeElements/Gridding/test/test_gridding.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     design = "ariane"
     src_dir = "../src"
     setup_file = "setup.tcl"
-    tolerance = 0.1
-    GriddingLefDefInterface(src_dir, design, setup_file, tolerance)
+    tolerance = 0.01
+    halo_width = 5.0
+    GriddingLefDefInterface(src_dir, design, setup_file, tolerance, halo_width)
 
