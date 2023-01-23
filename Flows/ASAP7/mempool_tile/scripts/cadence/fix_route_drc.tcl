8dfc16b1f95acda8498130eb05e6eb3591cfdc6f
sakundu <sakundu@ucsd.edu>
2023-01-23 11:05:24 -0800

    Added missing header
    Signed-off-by: sakundu <sakundu@ucsd.edu>

--- Flows/ASAP7/mempool_tile/scripts/cadence/fix_route_drc.tcl
+++ Flows/ASAP7/mempool_tile/scripts/cadence/fix_route_drc.tcl
@@ -1,3 +1,5 @@
+# This script was written and developed by ABKGroup students at UCSD. However, the underlying commands and reports are copyrighted by Cadence. 
+# We thank Cadence for granting permission to share our research to help promote and foster the next generation of innovators.
 deselectAll
 foreach ptr [dbget top.markers ]  {
     set msg [dbget ${ptr}.message]
